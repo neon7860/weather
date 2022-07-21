c3d002d13ea998846975e66f7fc01118269f9fe6
Muhammad Ibrahim <neon7860@gmail.com>
2022-07-21 18:37:01 +0100

    initial commit

---
+++ my-travel-journal/src/data.js
@@ -0,0 +1,52 @@
+export default [
+    {
+        id: 1,
+        title: "Guggenheim",
+        location: "SPAIN",
+        googleMapsUrl: "https://g.page/guggenheimbilbaomuseoa?share",
+        startDate: "24 Aug, 2019",
+        endDate: "24 Aug, 2020",
+        description: "The Guggenheim is a popular visiting site in the basque country. It an art museum in Bilbao, Spain, which features permanent and visiting exhibits of works by Spanish and international artists.",
+        imageUrl: "Bilbao.jpg"
+    },
+    {
+        id: 2,
+        title: "Stonehenge",
+        location: "ENGLAND",
+        googleMapsUrl: "https://goo.gl/maps/w6Q2p4i3WbqjNYBy6",
+        startDate: "Mar, 2010",
+        endDate: "Mar, 2010",
+        description: "Stonehenge is a prehistoric monument on Salisbury Plain in Wiltshire, England. One of the most famous landmarks in the United Kingdom, Stonehenge is regarded as a British cultural icon. Archaeologists believe that Stonehenge was constructed from 3000 BC to 2000 BC.",
+        imageUrl: "Stonehenge.jpg"
+    },
+    {
+        id: 3,
+        title: "Sunamganj",
+        location: "BANGLADESH",
+        googleMapsUrl: "https://goo.gl/maps/gEvEbngXgMDYAqbh7",
+        startDate: "2007",
+        endDate: "2007",
+        description: "Sunamganj is a town in the Sylhet Division of northeastern Bangladesh. It is the administrative headquarters and largest town of Sunamganj District. It is located on the banks of the Surma River, approximately 50 kilometres west-northwest of Sylhet, the divisional headquarters.",
+        imageUrl: "Sunamganj.jpg"
+    },
+    {
+        id: 4,
+        title: "Orbaneja del castillo",
+        location: "SPAIN",
+        googleMapsUrl: "https://goo.gl/maps/Wx5CQ6HFxYz6cBpe9",
+        startDate: "July, 2020",
+        endDate: "July, 2020",
+        description: "Orbaneja del Castillo is a hamlet and minor local entity located in the municipality of Valle de Sedano, in Burgos province, Castile and León, Spain.",
+        imageUrl: "Orbaneja.jpg"
+    },
+    {
+        id: 5,
+        title: "La playa de Sardinero",
+        location: "SPAIN",
+        googleMapsUrl: "https://goo.gl/maps/pvLbjWWdhY2iLppc6",
+        startDate: "July, 2020",
+        endDate: "July, 2020",
+        description: "The Sardinero is one of the most famous beaches of the coast of Cantabria and is the main beach of Santander. The water is shallow, so it has become a family beach. In the past, it was a beach exclusively for high-class people.",
+        imageUrl: "Santander.jpg"
+    }
+]
